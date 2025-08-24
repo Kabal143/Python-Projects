382a501bb5c228917568aa99d55d87208fe8dc29
Kabal143 <trishamaemellendez15@gmail.com>
2025-08-24 12:14:59 +0800

    Add hurdle-4.py

---
+++ hurdle-4.py
@@ -0,0 +1,34 @@
+def turn_right():
+    for i in range(3):
+        turn_left()
+
+def jump():
+    for i in range(1 < 14):
+        move()
+    if right_is_clear():
+        turn_right()
+
+while not at_goal():
+    if wall_in_front() and wall_on_right():
+        turn_left()
+    elif front_is_clear():
+        jump()
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
+# def turn_right():
+#     for i in range(3):
+#         turn_left()
+
+# def moving():
+#     for i in range(1 < 5):
+#         move()
+#     if right_is_clear():
+#         turn_right()
+        
+# while not at_goal():
+#     if wall_in_front():
+#         turn_left()
+#     elif front_is_clear():
+#         moving()
